7eeb22e00d6c19e781685a565c1c9c983dc5c3cd
alexzhang1030 <1642114555@qq.com>
2023-06-20 00:42:50 +0800

    feat(nvim): add autosave and ccc

---
+++ tag-nvim-lazy/config/nvim/lua/plugins/ccc.lua
@@ -0,0 +1,13 @@
+-- color picker and hightling
+-- picker: CccPick
+return {
+  {
+    "uga-rosa/ccc.nvim",
+    opts = {
+      highlighter = {
+        auto_enable = true,
+        lsp = true,
+      },
+    },
+  },
+}
