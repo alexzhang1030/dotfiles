e0df948980ea576b4d43892ffcdc385d54dc09a9
alexzhang1030 <1642114555@qq.com>
2023-06-24 13:10:04 +0800

    feat: update

--- tag-wezterm/config/wezterm/config/font.lua
+++ tag-wezterm/config/wezterm/config/font.lua
@@ -3,6 +3,7 @@ local wezterm = require("wezterm")
 return {
 	font = wezterm.font_with_fallback({
 		"MonoLisa Nerd Font Mono",
+		"PingFang SC",
 		"Maple Mono NF",
 		"FiraCode Nerd Font Mono",
 	}),
