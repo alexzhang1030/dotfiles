6e9c82aa71546b11a91ae04f89f2adcbf1c19aef
Alex <alexzhang1030@foxmail.com>
2025-03-07 00:33:57 +0800

    chore: update

--- tag-nvim-lazy/config/nvim/lua/plugins/editor.lua
+++ tag-nvim-lazy/config/nvim/lua/plugins/editor.lua
@@ -122,13 +122,13 @@ return {
       open_for_directories = false,
     },
   },
-  -- -- better error
-  -- {
-  --   "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-  --   name = "lsp_lines",
-  --   event = "VeryLazy",
-  --   config = function()
-  --     require("lsp_lines").setup()
-  --   end,
-  -- },
+  {
+    "linrongbin16/gitlinker.nvim",
+    cmd = "GitLink",
+    opts = {},
+    keys = {
+      { "<leader>go", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Yank git link" },
+      { "<leader>gO", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
+    },
+  },
 }
